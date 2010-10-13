3cfd48f0eb0eb188671bc4e8f45e951585ba2fd7
(no author) <(no author)@d56c34e7-b9f2-4d4c-b16c-3c6f6beb2c86>
2010-10-13 13:13:19 +0000

    Update README Added css class "action" to buttons so they use standard SilverStripe button styling in the admin
    git-svn-id: http://svn.gpmd.net/svn/open/multiselectfield/trunk@153 d56c34e7-b9f2-4d4c-b16c-3c6f6beb2c86

--- javascript/multiselectfield.js
+++ javascript/multiselectfield.js
@@ -8,7 +8,7 @@
         // Add source, destination and control fields
         sourceField.addClass('hidden');
         sourceField.before('<select class="multiselect-unselected" multiple="multiple"></select>');
-        sourceField.before('<div class="multiselect-controls"><p><button class="multiselect-add">Add &gt;</button></p><p><button class="multiselect-remove">&lt; Remove</button></p></div>');
+        sourceField.before('<div class="multiselect-controls"><p><button class="action multiselect-add">Add &gt;</button></p><p><button class="action multiselect-remove">&lt; Remove</button></p></div>');
         sourceField.before('<select class="multiselect-selected" multiple="multiple"></select>');
         
         // Move unselected items to source copy selected items to dest
